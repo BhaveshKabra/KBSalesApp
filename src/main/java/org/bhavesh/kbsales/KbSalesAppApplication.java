73a57103c5f949ef8fc08789a19d79afa096db98
Bhavesh Kabra <BhaveshKabra@users.noreply.github.com>
2020-05-21 13:07:25 +0530

    Add files via upload

---
+++ src/main/java/org/bhavesh/kbsales/KbSalesAppApplication.java
@@ -0,0 +1,13 @@
+package org.bhavesh.kbsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbSalesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbSalesAppApplication.class, args);
+	}
+
+}
